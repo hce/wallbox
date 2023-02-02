839eb8d40d5dec62daae114454debc95326bcb29
Hans-Christian Esperer <hc@hcesperer.org>
2023-02-02 23:54:08 +0100

    configuration foo

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ use clap::Parser;
 use e3dc::E3DC;
 use mennekes::Mennekes;
 use pac2200::Pac2200;
+use timeouter::Timeouter;
+
+const MODBUS_DEFAULT_PORT: u16 = 502;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = "Wallbox charging manager")]
@@ -32,32 +35,67 @@ fn main() {
         toml::from_str(&config_file).expect("TOML parsing")
     };
 
-    let e3dc = E3DC::new("localhost", 5020, std::time::Duration::from_secs(2))
-        .expect("Create e3dc object");
-    let mennekes = Mennekes::new("localhost", 5021, std::time::Duration::from_secs(60))
-        .expect("Create mennekes object");
-    let pac2200 = Pac2200::new("localhost", 5022, std::time::Duration::from_secs(1))
-        .expect("Create pac2200 object");
+    let e3dc = E3DC::new(
+        &config.e3dc.host,
+        config.e3dc.port.unwrap_or(MODBUS_DEFAULT_PORT),
+        std::time::Duration::from_secs(2),
+    )
+    .expect("Create e3dc object");
+    let mennekes = Mennekes::new(
+        &config.wallbox.host,
+        config.wallbox.port.unwrap_or(MODBUS_DEFAULT_PORT),
+        std::time::Duration::from_secs(60),
+    )
+    .expect("Create mennekes object");
+    let pac2200 = if let Some(pac_conn) = &config.pac2200 {
+        Some(
+            Pac2200::new(
+                &pac_conn.host,
+                pac_conn.port.unwrap_or(MODBUS_DEFAULT_PORT),
+                std::time::Duration::from_secs(1),
+            )
+            .expect("Create pac2200 object"),
+        )
+    } else {
+        None
+    };
 
     let mut e3dcparams;
     let mut mennekesparams;
     let mut pac2200params;
+    let t1 = Timeouter::new(config.initial_connection_timeout);
     loop {
         if let Some(n) = e3dc.get_current_params() {
             e3dcparams = n;
             break;
         }
+        if !t1.ok() {
+            eprintln!("Timeout while making initial connection to PV system");
+            std::process::exit(10);
+        }
     }
+    let t2 = Timeouter::new(config.initial_connection_timeout);
     loop {
         if let Some(n) = mennekes.get_current_params() {
             mennekesparams = n;
             break;
         }
+        if !t2.ok() {
+            eprintln!("Timeout while making initial connection to wallbox");
+            std::process::exit(11);
+        }
     }
-    loop {
-        if let Some(n) = pac2200.get_current_params() {
-            pac2200params = n;
-            break;
+    if let Some(pac2200) = &pac2200 {
+        let t3 = Timeouter::new(config.initial_connection_timeout);
+        loop {
+            if let Some(n) = pac2200.get_current_params() {
+                pac2200params = Some(n);
+                break;
+            }
+            if !t3.ok() {
+                eprintln!("Timeout while making initial connection to PAC2200");
+                std::process::exit(12);
+            }
         }
     }
     loop {
@@ -67,8 +105,10 @@ fn main() {
         if let Some(n) = mennekes.get_current_params() {
             mennekesparams = n;
         }
-        if let Some(n) = pac2200.get_current_params() {
-            pac2200params = n;
+        if let Some(pac2200) = &pac2200 {
+            if let Some(n) = pac2200.get_current_params() {
+                pac2200params = Some(n);
+            }
         }
 
         if mennekesparams.control_pilot == 0 {
